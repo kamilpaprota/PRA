7f4743c4da170908fb29ac8d37ef2c6118d00237
s416267 <kampap@st.amu.edu.pl>
2017-10-27 12:40:55 +0200

    test commit

--- PracowaniaProgramowania/src/main/java/third/quartz/JobMapScheduler.java
+++ PracowaniaProgramowania/src/main/java/third/quartz/JobMapScheduler.java
@@ -6,9 +6,13 @@ import org.quartz.SchedulerException;
 import org.quartz.Trigger;
 import org.quartz.impl.StdSchedulerFactory;
 
+
 import static org.quartz.JobBuilder.newJob;
 import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
 import static org.quartz.TriggerBuilder.newTrigger;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.CronScheduleBuilder.*;
+import static org.quartz.DateBuilder.*;
 
 public class JobMapScheduler {
 
@@ -26,11 +30,15 @@ public class JobMapScheduler {
 
             // Trigger the job to run now, and then repeat every 40 seconds
             Trigger trigger = newTrigger()
-                    .withIdentity("trigger1", "group1")
+                    /*.withIdentity("trigger1", "group1")
                     .startNow()
                     .withSchedule(simpleSchedule()
                             .withIntervalInSeconds(1)
                             .repeatForever())
+                    .build();*/
+                    .withIdentity("trigger1", "group1")
+                    .startNow()
+                    .withSchedule(cronSchedule("0 * * ? * MON-FRI"))
                     .build();
 
 
@@ -41,9 +49,9 @@ public class JobMapScheduler {
             scheduler.start();
 
 
-            Thread.sleep(6000);
+            //Thread.sleep(10000);
 
-            scheduler.shutdown();
+            //scheduler.shutdown();
 
         } catch (SchedulerException se) {
             se.printStackTrace();
--- PracowaniaProgramowania/src/main/java/third/quartz/JobWithMap.java
+++ PracowaniaProgramowania/src/main/java/third/quartz/JobWithMap.java
@@ -1,28 +1,157 @@
 package third.quartz;
-
+import java.util.Date;
 import org.quartz.*;
 
-@PersistJobDataAfterExecution
-public class JobWithMap  implements org.quartz.Job{
 
-    public void execute(JobExecutionContext context) throws JobExecutionException {
+@PersistJobDataAfterExecution
+public class JobWithMap  implements org.quartz.Job
+{
+    public int minutes_to_end;
+    public void execute(JobExecutionContext context) throws JobExecutionException
+    {
         {
             JobKey key = context.getJobDetail().getKey();
 
             JobDataMap dataMap = context.getJobDetail().getJobDataMap();
 
-            //read values
-            String jobSays = dataMap.getString("jobSays");
-            int counter = 0;
-            if (dataMap.containsKey("counter")) {
-                counter = dataMap.getInt("counter");
+            System.out.println(context.getFireTime());
+
+            this.minutes_to_end = -1;
+            boolean przerwa = getBreak(context.getFireTime().getHours(), context.getFireTime().getMinutes());
+
+            if(this.minutes_to_end != -1) {
+                if (przerwa) {
+                    System.out.println(this.minutes_to_end + " minut zostało do końca przerwy.");
+                } else {
+                    System.out.println(this.minutes_to_end + " minut zostało do końca zajęć.");
+                }
             }
-
-            System.err.println("Instance " + key + " of says: " + jobSays + " number " + counter);
-
-            counter++;
-            //increment and save counter
-            dataMap.put("counter", counter);
+            else
+                System.out.println("Nie ma aktualnie zajęć.");
         }
+    }
+
+    public boolean getBreak(int hours, int minutes)
+    {
+        boolean przerwa = false;
+        switch(hours)
+        {
+            case 8:
+                if(minutes >= 15) {
+                    przerwa = false;
+                    this.minutes_to_end = 105;
+                    this.minutes_to_end -= minutes;
+                }
+                break;
+            case 9:
+                if(minutes < 45) {
+                    this.minutes_to_end = 45;
+                    this.minutes_to_end -= minutes;
+                    przerwa = false;
+                }
+                else
+                {
+                    this.minutes_to_end = 60;
+                    this.minutes_to_end -= minutes;
+                    przerwa = true;
+                }
+                break;
+            case 10:
+                this.minutes_to_end = 90;
+                this.minutes_to_end -= minutes;
+                przerwa = false;
+                break;
+            case 11:
+                przerwa = false;
+                if(minutes < 30) {
+                    this.minutes_to_end = 30;
+                    this.minutes_to_end -= minutes;
+                }
+                else if(minutes >= 45)
+                {
+                    this.minutes_to_end = 135;
+                    this.minutes_to_end -= minutes;
+                }
+                else
+                {
+                    this.minutes_to_end = 45;
+                    this.minutes_to_end -= minutes;
+                    przerwa = true;
+                }
+                break;
+            case 12:
+                przerwa = false;
+                this.minutes_to_end = 75;
+                this.minutes_to_end -= minutes;
+                break;
+            case 13:
+                przerwa = false;
+                if(minutes < 15) {
+                    this.minutes_to_end = 15;
+                    this.minutes_to_end -= minutes;
+                }
+                else if(minutes >= 45) {
+                    this.minutes_to_end = 135;
+                    this.minutes_to_end -= minutes;
+                }
+                else
+                {
+                    przerwa = true;
+                    this.minutes_to_end = 45;
+                    this.minutes_to_end -= minutes;
+                }
+                break;
+            case 14:
+                przerwa = false;
+                this.minutes_to_end = 75;
+                this.minutes_to_end -= minutes;
+                break;
+            case 15:
+                przerwa = false;
+                if(minutes < 15) {
+                    this.minutes_to_end = 15;
+                    this.minutes_to_end -= minutes;
+                }
+                else if(minutes >= 30) {
+                    this.minutes_to_end = 120;
+                    this.minutes_to_end -= minutes;
+                }
+                else
+                {
+                    przerwa = true;
+                    this.minutes_to_end = 30;
+                    this.minutes_to_end -= minutes;
+                }
+                break;
+            case 16:
+                przerwa = false;
+                this.minutes_to_end = 60;
+                this.minutes_to_end -= minutes;
+                break;
+            case 17:
+                if(minutes < 15) {
+                    przerwa = true;
+                    this.minutes_to_end = 15;
+                    this.minutes_to_end -= minutes;
+                }
+                else
+                {
+                    przerwa = false;
+                    this.minutes_to_end = 105;
+                    this.minutes_to_end -= minutes;
+                }
+                break;
+            case 18:
+                if(minutes <= 45)
+                {
+                    this.minutes_to_end = 45;
+                    this.minutes_to_end -= minutes;
+                }
+                break;
+            default:
+                this.minutes_to_end = -1;
+                break;
+        }
+        return przerwa;
     }
 }
